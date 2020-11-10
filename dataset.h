18b8fb3fdaab7ec7411b41b74e47ec4b71860df2
KZNS <kzns@foxmail.com>
2020-11-11 00:17:52 +0800

    feat: add Dataset.readin_*()

--- dataset.h
+++ dataset.h
@@ -7,10 +7,14 @@
 class Dataset
 {
 private:
-    std::vector<Matrix> datas;
+    char read_char(std::ifstream &fin);
+    int read_int(std::ifstream &fin);
+    double read_double(std::ifstream &fin);
 public:
     int size, row, col;
-    int readin(const std::string &file_name);
+    std::vector<Matrix> datas;
+    int readin_images(const std::string &file_name);
+    int readin_labels(const std::string &file_name);
 };
 
 #include "dataset.cpp"
--- hwnr.cpp
+++ hwnr.cpp
@@ -3,15 +3,19 @@
 #include "dataset.h"
 using namespace std;
 
+Dataset x_train, y_train, x_test, y_test;
 int main()
 {
     //readin data
-    Dataset x_train, y_train, x_test, y_test;
-    x_train.readin("data/train-images-idx3-ubyte.gz");
-    y_train.readin("data/train-labels-idx1-ubyte.gz");
-    x_test.readin("data/t10k-images-idx3-ubyte.gz");
-    y_test.readin("data/t10k-labels-idx1-ubyte.gz");
-    
+    x_train.readin_images("data/train-images-idx3-ubyte");
+    cout << x_train.datas[0];
+    y_train.readin_labels("data/train-labels-idx1-ubyte");
+    cout << y_train.datas[0];
+    x_test.readin_images("data/t10k-images-idx3-ubyte");
+    cout << x_test.datas[0];
+    y_test.readin_labels("data/t10k-labels-idx1-ubyte");
+    cout << y_test.datas[0];
+
     //init network
     Network net;
     net.add_layer(28 * 28);
