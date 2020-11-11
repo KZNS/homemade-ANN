c9b680ce60aa18be4ae93d901a336976fe6e890f
KZNS <kzns@foxmail.com>
2020-11-11 16:42:21 +0800

    feat: add Network.fit() framework

--- network.cpp
+++ network.cpp
@@ -19,6 +19,31 @@ int Network::random_list(std::vector<int> &ls, int n)
     }
     return 0;
 }
+int Network::fit(const Matrix &x, const Matrix &y)
+{
+    z[0] = w[0] * x + b[0];
+    a[0] = activation(z[0]);
+    for (int i = 1; i < deep; i++)
+    {
+        z[i] = w[i] * a[i-1] + b[i];
+        a[i] = activation(z[i]);
+    }
+    delta[deep-1] = (a[deep-1]-y).hadamard(d_activation(z[deep-1]));
+    for (int i = deep-2; i >= 0; i--)
+    {
+        delta[i] = (w[i+1].T()*delta[i+1]).hadamard(d_activation(z[i]));
+    }
+    for (int i = 0; i < deep; i++)
+    {
+        b[i] -= delta[i];//可以乘学习率
+    }
+    w[0] -= x*delta[0];
+    for (int i = 1; i < deep; i++)
+    {
+        w[1] -= a[i-1] * delta[i];
+    }
+    return 0;
+}
 Network::Network()
 {
     input_shape = 0;
@@ -51,6 +76,13 @@ int Network::add_layer(int size)
     b[deep - 1].random();
     return 0;
 }
+int Network::init()
+{
+    z.resize(deep);
+    a.resize(deep);
+    delta.resize(deep);
+    return 0;
+}
 int Network::fit(const Dataset &x, const Dataset &y)
 {
     std::vector<int> ls;
@@ -58,7 +90,7 @@ int Network::fit(const Dataset &x, const Dataset &y)
     for (int i = 0; i < x.size; i++)
     {
         printf("\r%d/%d", i+1, x.size);
-        //fit(x.datas[ls[i]], y.datas[ls[i]]);
+        fit(x.datas[ls[i]], y.datas[ls[i]]);
     }
     printf("\n");
     return 0;
--- network.h
+++ network.h
@@ -9,8 +9,10 @@ class Network
 {
 private:
     std::vector<Matrix> w, b;
+    std::vector<Matrix> z, a, delta;
     int random_list(std::vector<int> &ls, int n);
-    int fix(const Matrix &x, const Matrix &y);
+    int fit(const Dataset &x, const Dataset &y);
+    int fit(const Matrix &x, const Matrix &y);
 
 public:
     Network();
@@ -19,7 +21,7 @@ public:
     int deep;
     int set_input_shape(int size);
     int add_layer(int size);
-    int fit(const Dataset &x, const Dataset &y);
+    int init();
     int fit(const Dataset &x, const Dataset &y, int epochs);
     int evaluate(const Dataset &x, const Dataset &y, double &test_loss, double &test_acc);
 };
